e5f5f49ec692cdad87e059f64cde683bf6c713b2
DESKTOP-OFV3LLK\Igor <vershininio@yandex.ru>
2018-10-19 10:17:20 +0300

    Initial commit

---
+++ src/main/java/ru/vershinin/chat/server/ChatController.java
@@ -0,0 +1,109 @@
+package ru.vershinin.chat.server;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("chat")
+public class ChatController {
+    private Queue<String> messages = new ConcurrentLinkedQueue<>();
+    private Map<String, String> usersOnline = new ConcurrentHashMap<>();
+
+    /**
+     * curl -X POST -i localhost:8080/chat/login -d "name=I_AM_STUPID"
+     */
+    @RequestMapping(
+            path = "login",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> login(@RequestParam("name") String name) {
+        if (name.length() < 1) {
+            return ResponseEntity.badRequest().body("Too short name, sorry :(");
+        }
+        if (name.length() > 20) {
+            return ResponseEntity.badRequest().body("Too long name, sorry :(");
+        }
+        if (usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("Already logged in:(");
+        }
+        usersOnline.put(name, name);
+        messages.add("[" + name + "] logged in");
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * curl -i localhost:8080/chat/online
+     */
+    @RequestMapping(
+            path = "online",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity online() {
+        String responseBody = String.join("\n", usersOnline.keySet().stream().sorted().collect(Collectors.toList()));
+        return ResponseEntity.ok(responseBody);
+    }
+
+    /**
+     * curl -X POST -i localhost:8080/chat/logout -d "name=I_AM_STUPID"
+     */
+    @RequestMapping(
+            path = "logout",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> logout(@RequestParam("name") String name) {
+        if (name.length() < 1) {
+            return ResponseEntity.badRequest().body("Too short name, sorry :(");
+        }
+        if (name.length() > 20) {
+            return ResponseEntity.badRequest().body("Too long name, sorry :(");
+        }
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("Name is not logged in :(");
+        }
+
+        usersOnline.remove(name);
+        messages.add("[" + name + "] logged out");
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * curl -X POST -i localhost:8080/chat/say -d "name=I_AM_STUPID&msg=Hello everyone in this chat"
+     */
+    @RequestMapping(
+            path = "say",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> say(@RequestParam("name") String name, @RequestParam("msg") String message) {
+        messages.add("[" + name + "] " + message);
+        return ResponseEntity.ok().build();
+    }
+
+
+    /**
+     * curl -i localhost:8080/chat/chat
+     */
+    @RequestMapping(
+            path = "chat",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity chat() {
+        String responseBody = String.join("\n", new ArrayList<>(messages));
+        return ResponseEntity.ok(responseBody);
+    }
+}
---
+++ src/test/java/ru/vershinin/chat/client/ChatClientTest.java
@@ -0,0 +1,46 @@
+package ru.vershinin.chat.client;
+
+import okhttp3.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ChatClientTest {
+    private static String MY_NAME_IN_CHAT = "IgorVershinin";
+    private static String MY_MESSAGE_TO_CHAT = "Hi everybody!";
+
+    @Test
+    public void login() throws IOException {
+        Response response = ChatClient.login(MY_NAME_IN_CHAT);
+        System.out.println("[" + response + "]");
+        String body = response.body().string();
+        System.out.println();
+        Assert.assertTrue(response.code() == 200 || body.equals("Already logged in:("));
+    }
+
+    @Test
+    public void viewChat() throws IOException {
+        Response response = ChatClient.viewChat();
+        System.out.println("[" + response + "]");
+        System.out.println(response.body().string());
+        Assert.assertEquals(200, response.code());
+    }
+
+
+    @Test
+    public void viewOnline() throws IOException {
+        Response response = ChatClient.viewOnline();
+        System.out.println("[" + response + "]");
+        System.out.println(response.body().string());
+        Assert.assertEquals(200, response.code());
+    }
+
+    @Test
+    public void say() throws IOException {
+        Response response = ChatClient.say(MY_NAME_IN_CHAT, MY_MESSAGE_TO_CHAT);
+        System.out.println("[" + response + "]");
+        System.out.println(response.body().string());
+        Assert.assertEquals(200, response.code());
+    }
+}
